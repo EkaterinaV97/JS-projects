086e511f43ab2fd85793c54f563d736c4b3773b0
Ekaterina <katemars18@gmail.com>
2022-05-24 19:37:36 +0500

    task Import from CSV

---
+++ Import from CSV/js/tasks.js
@@ -0,0 +1,30 @@
+/* tasks.js */
+
+export default class Tasks {
+  constructor() {
+    this.tasks = [];
+  }
+
+  /** @param {string} csvString */
+  importCsv(csvString) {
+    this.tasks = csvString.split(', ');
+    return this.tasks;
+  }
+
+  getCount() {
+    return this.tasks.length;
+  }
+
+  getFirst() {
+    return this.tasks[0];
+  }
+
+  getLast() {
+    return this.tasks[this.tasks.length - 1];
+  }
+
+  getUnformattedTasks() {
+    return this.tasks.join(', ').toLowerCase();
+  }
+
+}
